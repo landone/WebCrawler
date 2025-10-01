b24a22509a2f60f610deffa4b999b6d6e01e1fb2
Landon Ellis <landon.ellis2@gmail.com>
2025-10-01 15:56:25 -0400

    LinkedIn: Print Job names

--- Crawler.py
+++ Crawler.py
@@ -4,12 +4,16 @@ class Crawler:
 
     def __init__(self):
         self.p = sync_playwright().start()
-        self.browser = self.p.chromium.launch(headless=False)
+        self.browser = self.p.chromium.launch_persistent_context(
+            user_data_dir="./user_data",
+            headless=False
+        )
         self.page = self.browser.new_page()
+        self.browser.pages[0].close()  # Close the default blank page
     
     def goto(self, url):
         self.page.goto(url)
-        self.page.wait_for_load_state("networkidle")
+        self.wait_for_load()
 
     def close(self):
         self.browser.close()
@@ -18,5 +22,11 @@ class Crawler:
     def find(self, selector):
         return self.page.query_selector_all(selector)
     
-    def wait_for_load(self, state="networkidle"):
+    def wait_for_load(self, state="load"):
         self.page.wait_for_load_state(state)
+
+    def get_url(self):
+        return self.page.url
+    
+    def expect_navigation(self):
+        return self.page.expect_navigation()
--- main.py
+++ main.py
@@ -1,24 +1,47 @@
 from Crawler import Crawler
-from urllib.parse import urljoin
+import time
 
-def crawl(url):
-    crawler = Crawler()
-    crawler.goto(url)
-    elements = crawler.find("a[data-test-selector='recommended-channel']")
-    selectedElement = None
-    for element in elements:
-        href = element.get_attribute("href")
-        element.evaluate("el => el.style.backgroundColor = 'yellow'")
-        if href == '/northernlion':
-            selectedElement = element
-    input("Press Enter to close the browser...")
-    if selectedElement:
-        selectedElement.click()
-        crawler.wait_for_load()
-    input("Press Enter to close the browser...")
-    crawler.close()
-    return 0
+def use_search_box(crawler : Crawler, query):
+    textBox = crawler.find("input[id^='jobs-search-box-keyword-id']")[0]
+    textBox.fill(query)
+    with crawler.expect_navigation():
+        textBox.press("Enter")
+
+def scroll_job_list(crawler : Crawler):
+    jobList = crawler.find("ul.semantic-search-results-list")[0]
+    height = jobList.evaluate("e => e.scrollHeight")
+    scrollPos = 0
+    while scrollPos < height:
+        scrollPos = min(height, scrollPos + 600)
+        jobList.evaluate(f"e => e.scrollTop = {scrollPos}")
+        time.sleep(0.05)
+    jobList.evaluate("e => e.scrollTop = 0")
+    time.sleep(1)
+
+def search_jobs(crawler : Crawler):
+    use_search_box(crawler, "software engineer")
+    #Load results by scrolling the list
+    scroll_job_list(crawler)
+    #Return jobs
+    return crawler.find("a.job-card-job-posting-card-wrapper__card-link")
+
+def get_job_title(job):
+    title = job.query_selector("div.job-card-job-posting-card-wrapper__title").query_selector("strong").inner_html()
+    start = title.find('>')
+    end = title.rfind('<')
+    return title[start+1:end]
 
 if __name__ == "__main__":
-    start_url = "https://twitch.com"
-    crawl(start_url)
+    url = "https://www.linkedin.com/jobs/"
+    crawler = Crawler()
+    crawler.goto(url)
+    
+    jobs = search_jobs(crawler)
+    for job in jobs:
+        print(get_job_title(job))
+        with crawler.expect_navigation():
+            job.click()
+        time.sleep(0.5)
+    
+    input("...")
+    crawler.close()
